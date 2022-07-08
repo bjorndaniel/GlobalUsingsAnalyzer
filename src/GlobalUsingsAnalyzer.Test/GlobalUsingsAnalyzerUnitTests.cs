05a91ac0370ab34028b148228b9d04822e692ea5
Daniel Sandberg <bjorndaniel@gmail.com>
2022-07-08 16:20:06 +0200

    Added solution

---
+++ src/GlobalUsingsAnalyzer.Test/GlobalUsingsAnalyzerUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = GlobalUsingsAnalyzer.Test.CSharpCodeFixVerifier<
+    GlobalUsingsAnalyzer.GlobalUsingsAnalyzerAnalyzer,
+    GlobalUsingsAnalyzer.GlobalUsingsAnalyzerCodeFixProvider>;
+
+namespace GlobalUsingsAnalyzer.Test
+{
+    [TestClass]
+    public class GlobalUsingsAnalyzerUnitTest
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+    {|#0:using System;|}
+    namespace ConsoleApplication1
+    {
+        class Class1
+        {   
+        }
+    }";
+
+            var fixtest = @"
+    namespace ConsoleApplication1
+    {
+        class Class1
+        {   
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic("GlobalUsingsAnalyzer").WithLocation(0);
+            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+        }
+    }
+}
